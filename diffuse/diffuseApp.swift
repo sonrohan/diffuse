b2aeb4dcdbfea0c06bbb2c0a332bd84b40aeaf63
sonrohan <harrison.rohan@proton.me>
2026-05-23 19:14:55 -0700

    Initial Commit

---
+++ diffuse/diffuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  diffuseApp.swift
+//  diffuse
+//
+//  Created by Rohan Harrison on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct diffuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
